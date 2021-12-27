fb99e6eb48b8683394c2527b2a3b342b2cc6434a
VeejaLiu <veejaliu@gmail.com>
2021-12-27 13:38:40 +0800

    添加JavaDoc

--- src/main/java/com/veeja/mapper/ScienceFictionAuthorMapper.java
+++ src/main/java/com/veeja/mapper/ScienceFictionAuthorMapper.java
@@ -6,7 +6,21 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
+/**
+ * Author Mapper
+ * @author liuweijia
+ */
 public interface ScienceFictionAuthorMapper extends BaseMapper<ScienceFictionAuthor> {
+    /**
+     * 获取全部作者信息
+     * @return
+     */
     List<ScienceFictionAuthor> selectAll();
+
+    /**
+     * 根据名字获取作者信息
+     * @param firstName
+     * @return
+     */
     ScienceFictionAuthor selectAuthorByFirstName(@Param("firstName") String firstName);
 }
